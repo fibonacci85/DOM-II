19cc84c28abb02754858ffa4c1ac18ea60d92818
Francis Bonacic <francisbonacic@icloud.com>
2020-09-01 14:22:21 -0700

    just starting out

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+const funBus = document.querySelector('.logo-heading')
+funBus.addEventListener('click', (event) => {
+event.target.style.color = "red";
+event.target.style.fontSize = "100px"
+})
